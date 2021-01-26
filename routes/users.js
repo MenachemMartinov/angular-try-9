2a11fe73afe481ab490e602ee4a2a631edc52a75
MenachemMartinov <70970712+MenachemMartinov@users.noreply.github.com>
2021-01-26 19:24:24 +0200

    add option to update user

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.post("/newUser", async (req, res) => {
   // validate users body
   const { error } = validateUser(req.body);
   if (error) {
-    return res.status(400).send('error 1' + error.details[0].message);
+    return res.status(400).send("error 1" + error.details[0].message);
   }
 
   // validate user doesn't user exist already
@@ -30,6 +30,44 @@ router.post("/newUser", async (req, res) => {
 
   await user.save();
   res.send(_.pick(user, ["_id", "name", "email"]));
+});
+
+router.put("/:id", auth, async (req, res) => {
+  // validate users body
+  const { error } = validateUser(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  try {
+    // update the user
+    let user = await User.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      req.body
+    );
+    if (!user) {
+      return res.status(404).send("The user with the given ID was not found");
+    }
+
+    user = await User.findOne({
+      _id: req.params.id,
+    });
+    res.send(user);
+  } catch (error) {
+    console.error("error catch", error);
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  const user = await User.findOneAndRemove({
+    _id: req.params.id,
+  });
+  if (!user) {
+    return res.status(404).send("The user with the given ID was not found");
+  }
+
+  res.send(user);
 });
 
 module.exports = router;
